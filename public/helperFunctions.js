538b6e1c581843032ab71119f262cce64829291f
Atul Sharma <2012atulsharma@gmail.com>
2024-07-11 01:35:23 +0530

    Revert "Add counter for each link usage"

--- public/helperFunctions.js
+++ public/helperFunctions.js
@@ -1,28 +1,21 @@
-const updateCounter = () => {
-  let counterElement = document.getElementById('counter');
-  let count = parseInt(counterElement.innerText) || 0;
-  counterElement.innerText = count + 1;
-}
-
 // manipulate responseField to render a formatted and appropriate message
 const renderResponse = (res) => {
-  updateCounter();
-  if(res.errors){
-    responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>"
-  } else {
-    responseField.innerHTML = `<p>Your shortened url is: </p><p> ${res.shortUrl} </p>`
+    if(res.errors){
+      responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>"
+    } else {
+      responseField.innerHTML = `<p>Your shortened url is: </p><p> ${res.shortUrl} </p>`
+    }
   }
-}
-
-// manipulate responseField to render an unformatted response
-const renderRawResponse = (res) => {
-  updateCounter();
-  if(res.errors){
-    responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>"
-  } else {
-    let structuredRes = JSON.stringify(res).replace(/,/g, ", \n")
-    structuredRes = `<pre>${structuredRes}</pre>`
-
-    responseField.innerHTML = `${structuredRes}`
+  
+  // manipulate responseField to render an unformatted response
+  const renderRawResponse = (res) => {
+    if(res.errors){
+      responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>"
+    } else {
+      let structuredRes = JSON.stringify(res).replace(/,/g, ", \n")
+      structuredRes = `<pre>${structuredRes}</pre>`
+  
+      responseField.innerHTML = `${structuredRes}`
+    }
   }
-}
+  
--- public/main.js
+++ public/main.js
@@ -1,12 +1,13 @@
+// Information to reach API
 const apiKey = 'f44beb51cb0b414ca4b1f203be60ff03';
 const url = 'https://api.rebrandly.com/v1/links';
 
+// Some page elements
 const inputField = document.querySelector('#input');
 const shortenButton = document.querySelector('#shorten');
 const responseField = document.querySelector('#responseField');
 
-let counter = 0;
-
+// AJAX functions
 const shortenUrl = () => {
   const urlToShorten = inputField.value;
   const data = JSON.stringify({destination: urlToShorten});
@@ -16,17 +17,17 @@ const shortenUrl = () => {
   
   xhr.onreadystatechange = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
-      renderResponse(xhr.response);
-      counter++;
-      document.getElementById('counter').innerText = `Link usage count: ${counter}`;
-    }
+  		renderResponse(xhr.response);
+		}
   }
   xhr.open('POST', url);
   xhr.setRequestHeader('Content-type', 'application/json');
-  xhr.setRequestHeader('apikey', apiKey);
+	xhr.setRequestHeader('apikey', apiKey);
   xhr.send(data);
 }
 
+
+// Clear page and call AJAX functions
 const displayShortUrl = (event) => {
   event.preventDefault();
   while(responseField.firstChild){
